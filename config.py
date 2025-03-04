58d9f30f6f4aeb6e2d11579da01fb8ccee74bc90
HenryVerissimo <henriquedevsp@gmail.com>
2025-03-04 12:13:17 -0300

    Adicionando configurações de desenvolvimento

---
+++ config.py
@@ -0,0 +1,26 @@
+import os, random, string
+
+class Config(object):
+    CSRF_ENABLE = True
+    SECRET = "e3c7b4591194b6eab2ad9028db8193828370c3e9416ab7bc67017a56b8629500"
+    TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    APP = None
+
+class DevelopmentConfig(Config):
+    TESTING = True
+    DEBUG = False
+    IP_HOST = "localhost"
+    PORT_HOST = 8000
+    URL_MAIN = "http://%s:%s" % (IP_HOST, PORT_HOST) #http://localhost:8000
+
+app_config = {
+    "development": DevelopmentConfig(),
+    "testing": None,
+    "production": None
+}
+
+app_active = os.getenv("FLASK_ENV")
+
+if app_active is None:
+    app_active = "development"
